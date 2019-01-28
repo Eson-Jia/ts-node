48e1bf3797ca9321461e895cdf7dfad16896c430
jiayuxin <13520723393@163.com>
2019-01-28 15:16:59 +0800

    测试es6中的async、promise、arrow function等

--- src/greet.ts
+++ src/greet.ts
@@ -1,3 +1,9 @@
 export function sayHello(name: string) {
     return `Hello from ${name}`;
 }
+
+export function TestPromise(echo: string): Promise<string> {
+    return new Promise(resolve => {
+        setTimeout(resolve, 1000, `${echo} ${echo}`);
+    });
+}
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
-import { sayHello } from "./greet";
+import { sayHello, TestPromise } from "./greet";
 
 console.log(sayHello("TypeScript"));
 
 const theMap = new Map<number, string>();
 theMap.set(1, 'one');
 console.log(theMap.get(1));
+const test = async () => {
+    console.log(await TestPromise('hello'));
+}
+
+test();

e249f9dadce8430684b7af8db1d70002605dbd81
jiayuxin <13520723393@163.com>
2019-01-28 16:11:59 +0800

    配置生成sourcemp以便于直接调试ts

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,14 @@
-var gulp = require("gulp");
-var ts = require("gulp-typescript");
-var tsProject = ts.createProject("tsconfig.json");
+const gulp = require("gulp");
+const ts = require("gulp-typescript");
+const sourcemaps = require('gulp-sourcemaps');
+
+const tsProject = ts.createProject("tsconfig.json");
 
 gulp.task("default", function () {
     return tsProject.src()
+
+        .pipe(sourcemaps.init())
         .pipe(tsProject())
-        .js.pipe(gulp.dest("dist"));
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest("dist"));
 });
